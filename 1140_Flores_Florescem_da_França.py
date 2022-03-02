ed8e065ecb338bfab3592f1c851e780914596679
Rafael Pinto <rafaelzez@gmail.com>
2022-03-02 14:58:40 -0300

    Update 1140_Flores_Florescem_da_França.py

--- 1140_Flores_Florescem_da_França.py
+++ 1140_Flores_Florescem_da_França.py
@@ -1,3 +1,17 @@
+'''
+Fiona sempre amou poesia, e recentemente descobriu uma forma poética fascinante. Tautogramas são um caso especial de aliteração, que é a ocorrência da 
+mesma letra no início de palavras adjacentes. Em particular, uma sentença é um tautograma se todas suas palavras começam com a mesma letra.
+
+Por exemplo, as seguintes sentenças são tautogramas:
+> Flowers Flourish from France
+> Sam Simmonds speaks softly
+> Peter pIckEd pePPers
+> truly tautograms triumph
+> Fiona quer deslumbrar seu namorado com uma carta romântica repleta desse tipo de sentenças. Por favor, ajude Fiona a verificar se cada sentença 
+que ela escreveu é um tautograma ou não.
+'''
+
+
 while True:
     sentence = input().lower().split()
 
