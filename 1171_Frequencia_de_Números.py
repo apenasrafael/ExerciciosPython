69c1638b90d249df7cfa5a0aee94cc0991117d01
Rafael Pinto <rafaelzez@gmail.com>
2022-03-02 15:07:09 -0300

    Update 1171_Frequencia_de_Números.py

--- 1171_Frequencia_de_Números.py
+++ 1171_Frequencia_de_Números.py
@@ -4,9 +4,13 @@ entrada de dados, ou seja, deve-se escrever cada um dos valores distintos que ap
 crescente de valor.
 '''
 
-l = []
+dicionario = dict()
 for i in range(int(input())):
-    l.append(int(input()))
-
-for item in set(l):
-    print(f'{item} aparece {l.count(item)} vez(es)')
+    numero = int(input())
+    if numero not in dicionario.keys():
+        dicionario[numero] = 1
+    else:
+        dicionario[numero] += 1
+    
+for item in sorted(dicionario.keys()):
+    print(f'{item} aparece {dicionario[item]} vez(es)')
